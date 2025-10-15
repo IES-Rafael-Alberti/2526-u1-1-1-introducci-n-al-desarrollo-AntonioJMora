f7e51e7b4109ed1a84c1eda68a80f6a38a7a1cb0
nmorcab2106 <amorac@iesrafaelalberti.es>
2025-10-15 19:23:32 +0200

    Creación de programa con python

---
+++ programa.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cual es su nombre? ")
+año_nacimiento = int(input("¿Cuál es su año de nacimiento? "))
+edad = 2025 - año_nacimiento
+
+print("Hola ",nombre,", tienes ",edad," años. Este programa está hecho en el lenguaje de programación: Python.")
